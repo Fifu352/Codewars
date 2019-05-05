78dc3cc47676dcb9a4c02afdcb936e67d4a13a52
Filip Pieronkiewicz <987654321fp@gmail.com>
2019-05-05 17:47:17 +0200

    Cleanup

--- src/com/company/A/Two_teams.java
+++ src/com/company/A/Two_teams.java
@@ -9,18 +9,14 @@ public class Two_teams {
         int offset = in.nextInt();
         int[] array_of_inputs = new int[array_size];
         int[] array_of_teams = new int[array_size];
-        long t1 = System.currentTimeMillis();
+
         for (int i = 0; i < array_size; i++) {
             array_of_inputs[i] = in.nextInt();
         }
 
-
         System.out.println(teams(array_size, offset, array_of_inputs, array_of_teams));
-        long t2 = System.currentTimeMillis();
 
-        System.out.println(t2 - t1);
     }
-
 
     static String teams(int array_size, int offset, int[] array_of_inputs, int[] array_of_teams) {
         int max = 0;
@@ -92,57 +88,3 @@ public class Two_teams {
         }
     }
 }
-
-
-/*
-  int[] array_of_results = new int[n];
-        int value = 1;
-        int index = 0;
-        int cache = 0;
-        int num_cache=0;
-        int [][] cache_array = new int[n][1];
-        String result = "";
-        while (m.size() > 0) {
-            if (m.size() > 2 * b) {
-                index = m.indexOf(Collections.max(m));
-                //cache_array[num_cache][0] = index - b + cache;
-                num_cache++;
-                for (int i = 0; i < 2 * b + 1; i++) {
-                    if (index - b + i > -1) {
-
-                        m.remove(index - b);
-                        array_of_results[index - b + cache] = value;
-                        cache++;
-                    }
-
-                }
-                //cache_array[num_cache][1]=cache;
-
-
-                cache=0;
-                if (value == 1) {
-                    value = 2;
-                } else {
-                    value = 1;
-                }
-            }else{
-                index = m.indexOf(Collections.max(m));
-                for (int i = 0; i < 2 * b + 1; i++) {
-                    if ( m.size() > 0 ) {
-                        m.remove(0);
-                        array_of_results[index - b + cache] = value;
-                        cache++;
-                    }
-
-                }
-
-
-            }
-        }
-
-
-        for (int i : array_of_results) {
-            result += i;
-        }
-        return result;
- */
