f9ae2a52afddb1a9cff37428acc835bc63536d1f
Filip Pieronkiewicz <987654321fp@gmail.com>
2019-05-05 19:57:51 +0200

    Change to sorted set

--- src/com/company/A/Two_teams.java
+++ src/com/company/A/Two_teams.java
@@ -1,89 +1,96 @@
 package com.company.A;
-
 import java.util.Scanner;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class Two_teams {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int array_size = in.nextInt();
         int offset = in.nextInt();
-        int[] array_of_inputs = new int[array_size];
+        SortedSet<Player> set_of_inputs = new TreeSet<Player>();
+        Player[] array_of_players = new Player[array_size];
         int[] array_of_teams = new int[array_size];
 
         for (int i = 0; i < array_size; i++) {
-            array_of_inputs[i] = in.nextInt();
+            array_of_players[i] = new Player(in.nextInt(), i);
+            set_of_inputs.add(array_of_players[i]);
         }
-
-        System.out.println(teams(array_size, offset, array_of_inputs, array_of_teams));
-
+        System.out.println(teams(array_size, offset, set_of_inputs, array_of_teams, array_of_players));
     }
 
-    static String teams(int array_size, int offset, int[] array_of_inputs, int[] array_of_teams) {
-        int max = 0;
+    static String teams(int array_size, int offset, SortedSet<Player> set_of_inputs, int[] array_of_teams, Player[] array_of_players) {
         int index = 0;
         int index_l;
         int index_r;
         int team = 1;
-        int temp_max = 999999999;
 
-        while (true) {
-            for (int i = 0; i < array_size; i++) {
-                if ( array_of_teams[i] == 0) {
-                    if(array_of_inputs[i] > max){
-                        max = array_of_inputs[i];
-                        index = i;
+        while (!set_of_inputs.isEmpty()) {
+            index = set_of_inputs.last().iterator;
+            index_l = index;
+            index_r = index;
+
+            for (int i = 0; i < offset + 1; i++) {
+                if (i == 0) {
+                    set_of_inputs.remove(array_of_players[index]);
+                    array_of_teams[index] = team;
+                } else {
+                    while (true) {
+                        if (index_r + i < array_size) {
+                            if (array_of_teams[index_r + i] == 0) {
+                                set_of_inputs.remove(array_of_players[index_r + i]);
+                                array_of_teams[index_r + i] = team;
+                                break;
+                            }
+                            index_r++;
+                        } else {
+                            break;
+                        }
                     }
-                }
-                if (max == temp_max) {
-                    break;
+                    while (true) {
+                        if (index_l - i > -1) {
+                            if (array_of_teams[index_l - i] == 0) {
+                                set_of_inputs.remove(array_of_players[index_l - i]);
+                                array_of_teams[index_l - i] = team;
+                                break;
+                            }
+                            index_l--;
+                        } else {
+                            break;
+                        }
+                    }
                 }
             }
-
-            if (max == 0) {
-                StringBuilder result = new StringBuilder();
-                for (int value : array_of_teams) {
-                    result.append(value);
-                }
-                return result.toString();
+            if (team == 2) {
+                team = 1;
             } else {
-                index_l = index;
-                index_r = index;
+                team = 2;
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (int value : array_of_teams) {
+            result.append(value);
+        }
+        return result.toString();
+    }
 
-                for (int i = 0; i < offset + 1; i++) {
-                    if (i == 0) {
-                        array_of_teams[index] = team;
-                    } else {
-                        while (true) {
-                            if (index_r + i < array_size) {
-                                if (array_of_teams[index_r + i] == 0) {
-                                    array_of_teams[index_r + i] = team;
-                                    break;
-                                }
-                                index_r++;
-                            } else {
-                                break;
-                            }
-                        }
-                        while (true) {
-                            if (index_l - i > -1) {
-                                if (array_of_teams[index_l - i] == 0) {
-                                    array_of_teams[index_l - i] = team;
-                                    break;
-                                }
-                                index_l--;
-                            } else {
-                                break;
-                            }
-                        }
-                    }
-                }
-                if (team == 2) {
-                    team = 1;
-                } else {
-                    team = 2;
-                }
-                temp_max = max - 1;
-                max = 0;
+    static class Player implements Comparable<Player> {
+        int value;
+        int iterator;
+
+        Player(int value, int iterator) {
+            this.value = value;
+            this.iterator = iterator;
+
+        }
+
+        public int compareTo(Player a) {
+            if (value > a.value) {
+                return 1;
+            } else if (value < a.value) {
+                return -1;
+            } else {
+                return 0;
             }
         }
     }
